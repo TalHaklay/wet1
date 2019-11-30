efc7d239535ff4c1d1182bc9a51b75931e49aff8
Oref1995 <or.efrat@campus.technion.ac.il>
2019-11-30 12:58:53 +0200

    add cmake and library.cpp

---
+++ library.cpp
@@ -0,0 +1,9 @@
+#include"library.h"
+#include"DataCenterManager.h"
+void *Init() {
+    DataCenterManager *DS = new DataCenterManager ();
+    return (void*)DS;
+}
+StatusType AddDataCenter(void *DS, int dataCenterID, int numOfServers) {
+    return ((DataCenterManager *)DS)-> AddDataCenter(dataCenterID, numOfServers);
+}
